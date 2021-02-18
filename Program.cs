73ee5e7aa6a36bc17af72827c765097836dd764b
ranjeevg <57777381+ranjeevg@users.noreply.github.com>
2021-02-17 19:25:55 -0800

    Add files via upload
    initial commit for a program to left-transpose Roman characters by one, preserving capitalization, to code / decode messages in a terminal interface.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using leftTransposingByOne.Models;
+using leftTransposingByOne.Helpers;
+
+namespace leftTransposingByOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!" + "\n");
+            Thread.Sleep(750);
+            Console.WriteLine("Please enter your coded text: ");
+
+            string codedText = Console.ReadLine();
+
+            TransposedTextConverter TextConverter = new TransposedTextConverter(codedText);
+
+            Console.WriteLine("\n" + "Thanks! Decoding message: ");
+            Thread.Sleep(750);
+            Console.WriteLine(TextConverter.DecodedText);
+
+            Thread.Sleep(2500);
+            Console.Write("\nThanks! See you later! Press any key to exit: ");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ TransposedTextConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using leftTransposingByOne.Helpers;
+
+namespace leftTransposingByOne.Models
+{
+    public class TransposedTextConverter
+    {
+        public TransposedTextConverter() { }
+        public TransposedTextConverter(string code)
+        {
+            CodedText = code;
+        }
+
+        // this is the number of entries in the dictionary
+        private readonly int DictionarySize = 52;
+        public string CodedText { get; set; }
+
+        public string DecodedText => DecodeText();
+        public string DecodeText()
+        {
+            string decoded = String.Empty;
+            for (int i = 0; i < CodedText.Length; i++)
+            {
+                // do changes if 
+                if (Helper.AlphabetNumberDictionary
+                    .TryGetValue(CodedText[i], out int index))
+                {
+                    // wraparound fix for a and A
+                    if (index == 1 || index == 27)
+                    {
+                        index += 26;
+                    }
+                    index -= 1;
+                    // index = Helper.Mod(index, DictionarySize);
+                    decoded += Helper.AlphabetNumberDictionary
+                        .FirstOrDefault(x => x.Value == index).Key;
+                }
+                else
+                {
+                    decoded += CodedText[i];
+                }
+            }
+
+            return decoded;
+        }
+
+
+    }
+}
